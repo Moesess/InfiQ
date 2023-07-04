8ca9264456a5a9eb488fc8976c84c51f4d8189df
Tomasz Kaszuba <tomek@kaszuba.eu>
2023-07-04 21:25:04 +0200

    Refactor of project

--- InfiQ_API/asgi.py
+++ InfiQ_API/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for TechInfiQ_API project.
+ASGI config for InfiQ_API project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechInfiQ_API.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InfiQ_API.settings')
 
 application = get_asgi_application()
--- InfiQ_API/wsgi.py
+++ InfiQ_API/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TechInfiQ_API project.
+WSGI config for InfiQ_API project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TechInfiQ_API.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InfiQ_API.settings')
 
 application = get_wsgi_application()
