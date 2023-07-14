a4da5d93a2dc2b2b37b169f16f80c321c2780601
Tomasz Kaszuba <tomek@kaszuba.eu>
2023-07-14 15:52:31 +0200

    no message

--- InfiQ_API/settings.py
+++ InfiQ_API/settings.py
@@ -160,9 +160,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 40,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ]
 }
 
 MEDIA_URL = '/media/'
