dc8f1e80d0d263a643703631ed0befa91edb84bc
Tomasz Kaszuba <tomek@kaszuba.eu>
2023-08-22 19:45:45 +0200

    LeanTwean animations for popups

--- InfiQ/Assets/Scripts/PopUps/PopUp.cs
+++ InfiQ/Assets/Scripts/PopUps/PopUp.cs
@@ -3,9 +3,25 @@ using UnityEngine;
 public class PopUp : MonoBehaviour
 {
     [SerializeField] GameObject Panel;
+    [SerializeField] GameObject Dialog;
 
     public void Close()
     {
-        Destroy(Panel);
+        CanvasGroup panelGroup = Panel.GetComponent<CanvasGroup>();
+        LeanTween.scale(Dialog, Vector3.zero, 0.5f).setEaseInElastic().setOnComplete(
+            () => LeanTween.alphaCanvas(panelGroup, 0f, 0.5f).setEase(LeanTweenType.easeOutSine).setOnComplete(() => Destroy(Panel))
+        );
+        
+    }
+
+    private void Start()
+    {
+        CanvasGroup panelGroup = Panel.GetComponent<CanvasGroup>();
+        panelGroup.alpha = 0;
+        Dialog.transform.localScale = Vector3.zero;
+
+        LeanTween.alphaCanvas(panelGroup, 1f, 0.5f).setEase(LeanTweenType.easeOutSine).setOnComplete(
+            () => LeanTween.scale(Dialog, Vector3.one, 0.5f).setEaseOutElastic()
+        );
     }
 }
