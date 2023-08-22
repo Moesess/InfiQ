bce75f76aebf498ee17b07a0956e20124fe805b4
Tomasz Kaszuba <tomek@kaszuba.eu>
2023-08-22 20:46:32 +0200

    Main Menu Animations

---
+++ InfiQ/Assets/Scripts/Animations/MainMenuAnimations.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MainMenuAnimations : MonoBehaviour
+{
+    [SerializeField] GameObject TopMenu;
+    [SerializeField] GameObject TestButton1;
+    [SerializeField] GameObject TestButton2;
+    [SerializeField] GameObject TestButton3;
+    [SerializeField] GameObject BottomButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TestButton1.GetComponent<CanvasGroup>().alpha = 0;
+        TestButton2.GetComponent<CanvasGroup>().alpha = 0;
+        TestButton3.GetComponent<CanvasGroup>().alpha = 0;
+        TopMenu.GetComponent<CanvasGroup>().alpha = 0;
+        BottomButtons.GetComponent<CanvasGroup>().alpha = 0;
+        
+        StartCoroutine(DelayedStart());
+    }
+
+    IEnumerator DelayedStart()
+    {
+        yield return null;
+        LeanTween.alphaCanvas(TopMenu.GetComponent<CanvasGroup>(), 1f, 0.5f).setEase(LeanTweenType.easeOutSine);
+
+        yield return null;
+        LeanTween.alphaCanvas(TestButton1.GetComponent<CanvasGroup>(), 1f, 0.5f).setEase(LeanTweenType.easeOutSine);
+        SlideInFromLeft(TestButton1);
+
+        yield return new WaitForSeconds(.1f);
+        LeanTween.alphaCanvas(TestButton2.GetComponent<CanvasGroup>(), 1f, 0.5f).setEase(LeanTweenType.easeOutSine);
+        SlideInFromLeft(TestButton2);
+
+        yield return new WaitForSeconds(.1f);
+        LeanTween.alphaCanvas(TestButton3.GetComponent<CanvasGroup>(), 1f, 0.5f).setEase(LeanTweenType.easeOutSine);
+        SlideInFromLeft(TestButton3);
+
+        yield return new WaitForSeconds(.1f);
+        LeanTween.alphaCanvas(BottomButtons.GetComponent<CanvasGroup>(), 1f, 0.5f).setEase(LeanTweenType.easeOutSine);
+    }
+
+    public void SlideInFromLeft(GameObject objToSlide)
+    {
+        // Store the original position
+        Vector3 originalPosition = objToSlide.transform.position;
+
+        // Set the starting position (e.g., off-screen to the left)
+        objToSlide.transform.position = new Vector3(-Screen.width, originalPosition.y, originalPosition.z);
+
+        // Slide the object to its original position over a duration of 0.5 seconds
+        LeanTween.move(objToSlide, originalPosition, 1f).setEaseInOutQuad();
+    }
+
+    public void SlideOutToRight(GameObject objToSlide)
+    {
+        // Calculate the target position (e.g., off-screen to the right)
+        Vector3 targetPosition = new Vector3(Screen.width, objToSlide.transform.position.y, objToSlide.transform.position.z);
+
+        // Slide the object to the target position over a duration of 0.5 seconds
+        LeanTween.move(objToSlide, targetPosition, 1f).setEaseInOutQuad();
+    }
+
+}
