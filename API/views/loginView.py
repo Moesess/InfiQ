ed27d280892c84975888d12ae7fa8958db3c4c90
Tomasz Kaszuba <tomek@kaszuba.eu>
2023-08-27 21:38:51 +0200

    API userid

--- API/views/loginView.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def LoginView(request):
-    return render(request, 'rest_framework/login.html')
--- InfiQ_API/urls.py
+++ InfiQ_API/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from django.contrib.auth import views as auth_views
 
 from API import views
 
@@ -20,8 +19,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('accounts/', include('allauth.urls')),
-    path('login/', views.LoginView, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('send-email/', views.SendEmailView.as_view(), name='send-email'),
 ]
 
